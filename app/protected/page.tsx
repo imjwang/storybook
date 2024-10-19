5110982728fc4ff7ee3654efe6b9154d9ca5807f
imjwang <ngjaywa@gmail.com>
2024-10-19 08:29:23 -0700

    linting

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,5 +1,4 @@
 import { createClient } from "@/lib/supabase/server";
-import { InfoIcon } from "lucide-react";
 import { redirect } from "next/navigation";
 
 export default async function ProtectedPage() {
