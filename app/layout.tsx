fcb2f2b423b76a5eadbc7e140892bdd092883338
imjwang <ngjaywa@gmail.com>
2024-10-18 23:25:49 -0700

    auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import HeaderAuth from "@/components/header-auth";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,6 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <HeaderAuth />
         {children}
       </body>
     </html>
